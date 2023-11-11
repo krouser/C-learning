50b43f02d64f522d778aa9aee7f745e581734644
Stanley-A <east4era@yahoo.com>
2023-11-11 01:27:17 +0100

    Create displayOddNnumbers.cpp

---
+++ displayOddNnumbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() 
+{
+    for (int i = 1; i <= 100; ++i)
+    {
+        if (i % 2 > 0)
+        std::cout << i <<   "is an odd number" << std::endl;
+            
+    }
+    return 0;
+}
